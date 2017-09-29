f6821b5e0ea553617e4c44cae8be91e732171d0c
Gennaro Anzalone <g.anzalone12@gmail.com>
2017-09-29 12:33:44 -0700

    add store entry form and brand entry form with landing pages

--- app.rb
+++ app.rb
@@ -3,25 +3,47 @@ require('pry')
 Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
-get('/') do
-  @stores = Store.all()
+get('/')do
   erb(:index)
 end
 
-get('/form')do
-  erb(:form)
+get('/stores') do
+  @stores = Store.all()
+  erb(:stores)
+end
+
+get('/brands')do
+  @brands = Brand.all()
+  erb(:brands)
+end
+
+get('/store_form')do
+  erb(:store_form)
+end
+
+get('/brand_form')do
+  erb(:brand_form)
 end
 
 post('/')do
   name = params.fetch('name')
   store = Store.new({:name => name})
   store.save()
-  erb(:index)
+  @stores = Store.all()
+  erb(:stores)
 end
 
 get('/store/:id') do
   id = params[:id].to_i
   @store = Store.find(id)
-  @brand = Brand.find(id)
   erb(:store_detail)
+end
+
+post('/')do
+  name = params.fetch('name')
+  params.fetch('price')
+  brand = Brand.new({:name => name, :price => price})
+  brand.save()
+  @brands = Brand.all()
+  erb(:brands)
 end
