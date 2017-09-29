e8ba628c74ce3cf5f77f33c60cf50063900271fb
Gennaro Anzalone <g.anzalone12@gmail.com>
2017-09-29 09:43:58 -0700

    begin to set up views

---
+++ app.rb
@@ -0,0 +1,9 @@
+require("bundler/setup")
+require('pry')
+Bundler.require(:default)
+Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  @stores = Stores.all
+  erb(:index)
+end
