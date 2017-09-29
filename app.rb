b3ca0a89b52e3922180dfa402e907068f9169374
Gennaro Anzalone <g.anzalone12@gmail.com>
2017-09-29 15:28:22 -0700

    re-do views and routing

--- app.rb
+++ app.rb
@@ -7,9 +7,43 @@ get('/')do
   erb(:index)
 end
 
-get('/stores') do
+get('/stores')do
   @stores = Store.all()
   erb(:stores)
+end
+
+post('/store_form')do
+  name = params.fetch('name')
+  store = Store.new({:name => name})
+  store.save()
+  redirect '/'
+end
+
+get('/store/:id') do
+  @store = Store.find(params[:id])
+  @brand = Brand.find(params[:id])
+  erb(:store_detail)
+end
+
+get('/stores/:id/edit') do
+  @store = Store.find(params[:id].to_i)
+  erb(:store_edit)
+end
+
+patch('/stores/:id') do
+  @section = 'stores'
+  @store = Store.find(params[:id].to_i)
+  if @store.update({name: params['name']})
+    redirect '/stores/' + @store.id.to_s
+  else
+    erb(:store_edit)
+  end
+end
+
+delete('/delete/:id') do
+  store_id = params[:id].to_i
+  Store.where(id: store_id).destroy_all
+  redirect to("/")
 end
 
 get('/brands')do
@@ -17,33 +51,19 @@ get('/brands')do
   erb(:brands)
 end
 
-get('/store_form')do
-  erb(:store_form)
-end
-
 get('/brand_form')do
   erb(:brand_form)
 end
 
-post('/')do
+post('/brand_form')do
   name = params.fetch('name')
-  store = Store.new({:name => name})
-  store.save()
-  @stores = Store.all()
-  erb(:stores)
-end
-
-get('/store/:id') do
-  id = params[:id].to_i
-  @store = Store.find(id)
-  erb(:store_detail)
-end
-
-post('/')do
-  name = params.fetch('name')
-  params.fetch('price')
+  price = params.fetch('price')
   brand = Brand.new({:name => name, :price => price})
   brand.save()
-  @brands = Brand.all()
   erb(:brands)
+end
+
+get('/brand/:id') do
+  @brand = Brand.find(params[:id])
+  erb(:brand_detail)
 end
